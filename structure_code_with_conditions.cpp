fabfb71e538ae90054a1b5b8334262b639cea384
WizzOFF <maher_off@proton.me>
2025-03-27 04:27:44 +0100

    structure_code_with_conditions.cpp: basic condition if else > other test with else if

--- structure_code_with_conditions.cpp
+++ structure_code_with_conditions.cpp
@@ -1,4 +1,4 @@
-
+/*
 #include <iostream>
 
 using namespace std;
@@ -18,5 +18,35 @@ int main()
 
     cout << "Fin du programme" << endl;
 
+    return 0;
+}
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int nbAnimaux(2);
+
+    if (nbAnimaux == 0)
+    {
+        cout << "Vous n'avez pas d'animaux de compagnie" << endl;
+    }
+    else if (nbAnimaux == 1)
+    {
+        cout << "C'est super d'avoir un fidele compagnon !" << endl;
+    }
+    else if (nbAnimaux == 2)
+    {
+        cout << "Ca doit en faire du monde a la maison !" << endl;
+    }
+    else
+    {
+        cout << "Votre budget croquettes doit etre important" << endl;
+    }
+
+    cout << "Fin du programme" << endl;
     return 0;
 }
