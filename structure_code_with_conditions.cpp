eccddc4321a7cfb36cd310b3d82974bc637ca88c
WizzOFF <maher_off@proton.me>
2025-03-27 04:32:36 +0100

    structure_code_with_conditions.cpp: more readable code with switch

--- structure_code_with_conditions.cpp
+++ structure_code_with_conditions.cpp
@@ -21,7 +21,7 @@ int main()
     return 0;
 }
 */
-
+/*
 #include <iostream>
 
 using namespace std;
@@ -48,5 +48,36 @@ int main()
     }
 
     cout << "Fin du programme" << endl;
+    return 0;
+}
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int nbAnimaux(2);
+
+    switch (nbAnimaux)
+    {
+        case 0:
+            cout << "Vous n'avez pas d'animaux de compagnie" << endl;
+            break;
+
+        case 1:
+            cout << "C'est super d'avoir un fidele compagnon !" << endl;
+            break;
+
+        case 2:
+            cout << "Ca doit en faire du monde a la maison !" << endl;
+            break;
+
+        default:
+            cout << "Votre budget croquettes doit etre important" << endl;
+            break;
+    }
+
     return 0;
 }
