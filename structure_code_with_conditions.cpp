dacc9cc05d5567d5fdf5ffcb646c972befa4cf83
WizzOFF <maher_off@proton.me>
2025-03-27 04:18:16 +0100

    structure_code_with_conditions.cpp: basic condition if else > open the condition with if

---
+++ structure_code_with_conditions.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int nbAnimaux(2);
+
+    if (nbAnimaux > 0)
+    {
+        cout << "Super ! Les animaux de compagnie ca change la vie !" << endl;
+    }
+
+    cout << "Fin du programme" << endl;
+    
+    return 0;
+}
